d0d2b8f2f47be1003ef4c8290083a2fc72d1e3d1
Vasiliy <kkolyasev1228@gmail.com>
2020-09-18 03:14:57 +0300

    home

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    protected $p_rep;
+    protected $s_rep;
+    protected $a_rep;
+    protected $m_rep;
+
+    protected $template;
+    protected $vars = [];
+
+    protected $contentRightBar = false;
+    protected $contentLefttBar = false;
+
+    protected $bar = false;
+
+    public function __construct()
+    {
+    }
+
+    protected function renderOutput() {
+        return view($this->template)->with($this->vars);
+    }
+}
