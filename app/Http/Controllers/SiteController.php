5f0128b4ca9d1ce92b214243859b62326a16ff08
Vasiliy <kkolyasev1228@gmail.com>
2020-09-18 20:20:03 +0300

    menu

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -31,7 +31,7 @@ class SiteController extends Controller
 
         $menu = $this->getMenu();
 
-        $navigation = view(env('THEME') . '.navigation')->render();
+        $navigation = view(env('THEME') . '.navigation', compact('menu'))->render();
         $this->vars = Arr::add($this->vars, 'navigation', $navigation);
         return view($this->template)->with($this->vars);
     }
@@ -39,6 +39,21 @@ class SiteController extends Controller
     protected function getMenu() {
         $menu = $this->m_rep->get();
 
-        return $menu;
+        $mBuilder = \Menu::make('MyNav', function ($m) use ($menu) {
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                }
+                else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id();
+                    }
+                }
+            }
+        });
+
+
+
+        return $mBuilder;
     }
 }
