c8eb39f58c9b81ea8493d12516bca24d85dbfe70
Vasiliy <kkolyasev1228@gmail.com>
2020-09-19 16:47:39 +0300

    slider

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\MenusRepository;
+use App\Repositories\SlidersRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Config;
 
 class IndexController extends SiteController
 {
 
-    public function __construct()
+    public function __construct(SlidersRepository $s_rep)
     {
         parent::__construct(new \App\Repositories\MenusRepository(new \App\Menu));
+
+        $this->s_rep = $s_rep;
 
         $this->bar = 'right';
         $this->template = env('THEME') . '.index';
@@ -20,10 +25,30 @@ class IndexController extends SiteController
      * Display a listing of the resource.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Throwable
      */
     public function index()
     {
+        $sliderItems = $this->getSliders();
+
+        $sliders = view(env('THEME') . '.slider')->with('sliders', $sliderItems)->render();
+        $this->vars = Arr::add($this->vars,'sliders', $sliders);
         return $this->renderOutput();
+    }
+
+    public function getSliders()
+    {
+        $sliders = $this->s_rep->get();
+        if ($sliders->isEmpty()) {
+            return false;
+        }
+        $sliders->transform(function ($item, $key) {
+            $item->img = Config::get('settings.slider_path') . '/' . $item->img;
+
+            return $item;
+        });
+
+        return $sliders;
     }
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Blade::directive('set', function ($like) {
+            list($name, $val) = explode(',', $like);
+
+            return "<?php $name = $val?>";
+        });
     }
 }
