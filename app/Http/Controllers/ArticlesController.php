c32bf0345eb827246ab97d251583a47b7a209570
Vasiliy <kkolyasev1228@gmail.com>
2020-09-20 22:40:35 +0300

    articles

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,18 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ArticlesRepository;
+use App\Repositories\PortfolioRepository;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
-class ArticlesController extends Controller
+class ArticlesController extends SiteController
 {
+
+    public function __construct(PortfolioRepository $p_rep, ArticlesRepository $a_rep)
+    {
+        parent::__construct(new \App\Repositories\MenusRepository(new \App\Menu));
+
+        $this->p_rep = $p_rep;
+        $this->a_rep = $a_rep;
+
+        $this->bar = 'right';
+        $this->template = env('THEME') . '.articles';
+    }
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory|View
      */
     public function index()
     {
-        //
+        $articles = $this->getArticles();
+
+        return $this->renderOutput();
+    }
+
+    protected function getArticles($alias = false) {
+        $articles = $this->a_rep->get(['title', 'alias', 'created_at', 'img', 'desc'], false, true);
+
+        if ($articles) {
+            //$articles->load('user', 'category', 'comments');
+        }
+
+        return $articles;
     }
 
     /**
--- config/settings.php
+++ config/settings.php
@@ -1,8 +1,7 @@
 <?php
 return [
     'slider_path' => 'slider-cycle',
-    'portfolio_path' => 'projects',
-    'articles_path' => 'articles',
     'home_port_count' => 5,
-    'home_articles_count' => 3
+    'home_articles_count' => 3,
+    'paginate' => 2,
 ];
