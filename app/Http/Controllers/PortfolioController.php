0b4b665d2d09246d42ed8876e16716240a301909
Vasiliy <kkolyasev1228@gmail.com>
2020-09-28 18:03:58 +0300

    new portfolio

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -2,18 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\PortfolioRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
-class PortfolioController extends Controller
+class PortfolioController extends SiteController
 {
+
+    public function __construct(PortfolioRepository $p_rep)
+    {
+        parent::__construct(new \App\Repositories\MenusRepository(new \App\Menu));
+
+        $this->p_rep = $p_rep;
+
+        $this->template = env('THEME') . '.portfolios';
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Throwable
      */
     public function index()
     {
-        //
+        $this->title = 'Портволио';
+        $this->keywords = 'Портволио';
+        $this->meta_desc = 'Портволио';
+
+        $content = view(env('THEME') . '.content_portfolios')->render();
+        $this->vars = Arr::add($this->vars, 'content', $content);
+
+        return $this->renderOutput();
     }
 
     /**
